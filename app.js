67adb0d15edd3168d6f3dd3b3dfa19eae0805594
plazmica10 <ognjenstancevic@gmail.com>
2022-10-28 22:11:38 +0200

    add start script

--- app.js
+++ app.js
@@ -110,6 +110,8 @@ app.use((err, req, res, next) => {
     if (!err.message) err.message = 'Something went wrong'
     res.status(status).render('error', { err })
 })
-app.listen(3000, () => {
-    console.log('PORT 3000')
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Port: ${port}`);
 })
